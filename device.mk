1810c02f35fd8cde1938b42579b22985ce242c37
Yuchen <starracingguo@gmail.com>
2026-03-25 23:01:01 +0800

    Update device.mk for OnePlus Canoe configuration

--- device.mk
+++ device.mk
@@ -8,7 +8,7 @@
 # SPDX-License-Identifier: GPL-3.0-or-later
 #
 
-LOCAL_PATH := device/oneplus/infiniti
+LOCAL_PATH := device/oneplus/canoe
 
 # Shipping API level
 BOARD_SHIPPING_API_LEVEL := 34
@@ -32,5 +32,5 @@ PRODUCT_EXTRA_RECOVERY_KEYS += \
 PRODUCT_SOONG_NAMESPACES += $(LOCAL_PATH)
 
 # some OrangeFox-specific settings
-$(call inherit-product, $(LOCAL_PATH)/fox_infiniti.mk)
+$(call inherit-product, $(LOCAL_PATH)/fox_canoe.mk)
 #
